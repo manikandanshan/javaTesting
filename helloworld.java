6e01cf941f5eb84fc93a70ceb6d69d3b5fd97074
manikandanshan <manikbhuvan1985@gmail.com>
2016-08-10 13:11:55 +0530

    committed

--- helloworld.java
+++ helloworld.java
@@ -1,9 +1,9 @@
 /* HelloWorld.java
  */
 
-public class HelloWorld
+public class helloworld
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World mani!");
 	}
 }
